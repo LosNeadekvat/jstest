4676b2c5dedd8865455e69d85302fa8e59ad34dd
LosNeadekvat <losneadekvat@yandex.ru>
2022-04-02 14:34:17 +0300

    Created project

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+import React from "react";
+import {Clock} from "./clock";
+
+function App() {
+    return (
+        <div>
+            <Clock/>
+            <Clock/>
+            <Clock/>
+        </div>
+    );
+}
+export default App;
---
+++ src/clock.js
@@ -0,0 +1,35 @@
+import React from "react";
+
+export class Clock extends React.Component {
+    state = {date: new Date(), counter:0};
+    componentDidMount() {
+        this.timerID = setInterval(
+            this.tick,
+            this.props.tm ?? 1000
+        );
+    }
+    componentWillUnmount() {
+        clearInterval(this.timerID);
+    }
+   tick = () => this.setState({date: new Date(), counter: this.state.counter+1});
+
+    render(){
+        return (
+            <>
+                <form onSubmit={()=>{}}/>
+                <div>
+                    <h1>Hello, world!{this.state.counter}</h1>
+                    <Dial date={this.state.date}/>
+                </div>
+            </>
+        );
+    }
+}
+
+class Dial extends React.Component {
+    render(){
+        return (
+            <h2>Now {this.props.date.toLocaleTimeString()}.</h2>
+        )
+    }
+}
